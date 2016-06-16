1e593c460d4c70c0f8d148572e24d51b09b3b097
niuxinhuai <1242537720@qq.com>
2016-06-16 21:13:22 +0800

    123

---
+++ CocopodCreate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocopodCreate
+//
+//  Created by mac on 16/6/16.
+//  Copyright © 2016年 ZhiYou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CocopodCreate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CocopodCreate
+//
+//  Created by mac on 16/6/16.
+//  Copyright © 2016年 ZhiYou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
